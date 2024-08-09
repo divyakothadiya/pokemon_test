46d88ae9043ae09060c11644a061123b8fa9841a
deepkothadiya13 <deepkothadiya@gmail.com>
2024-08-10 04:27:13 +0530

    database cred push

--- pokmon_battle/pokmon_battle/settings.py
+++ pokmon_battle/pokmon_battle/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pokmon_battle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database-1',
+        'USER': 'admin',
+        'PASSWORD': 'Admin12345',
+        'HOST': 'database-1.cb6ce2qu0vga.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',  # Default MySQL port
     }
 }
 
