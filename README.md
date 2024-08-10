378a605499f4feeafcb26ee87c51d2a03af78375
deepkothadiya13 <deepkothadiya@gmail.com>
2024-08-11 00:07:25 +0530

    update read me and clean up

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# Pokémon Battle Simulator
+## Setup
+
+1. Clone the repository:
+   ```bash
+   git clone <repository-url>
+   cd pokemon_battle
+
+
 there are 3 endpoint as per below
 1. get api/pokemon (including pagination: api/pokemon?page=2)
   "next": "<instance>/api/pokemon?page=3",
--- pokmon_battle/pokmon_battle/urls.py
+++ pokmon_battle/pokmon_battle/urls.py
@@ -16,27 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Pokémon Battle API",
-      default_version='v1',
-      description="API documentation for the Pokémon Battle Simulator",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@example.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-   urlconf='pokemon_battle.urls'
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('battle.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
